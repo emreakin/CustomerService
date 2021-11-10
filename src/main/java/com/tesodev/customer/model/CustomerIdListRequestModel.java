afb36197cf9b611cd0a5ff40c645ee3ac6dc0095
emreakin <eakin777@gmail.com>
2021-11-10 11:30:40 +0300

    default const fix

--- src/main/java/com/tesodev/customer/model/CustomerIdListRequestModel.java
+++ src/main/java/com/tesodev/customer/model/CustomerIdListRequestModel.java
@@ -6,8 +6,10 @@ import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CustomerIdListRequestModel implements Serializable {
 
